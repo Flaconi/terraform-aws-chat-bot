183103a07f3428ed1721ae2589b58f68dc9fcb1a
waqas.yousaf <waqas.yousaf@flaconi.de>
2026-04-15 16:56:57 +0200

    OPS-7240: fixed example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,10 +1,10 @@
 module "chatbot" {
   source = "../../"
 
-  name             = "sample-alerts"
-  iam_role_arn     = "arn:aws:iam::123456789012:role/aws-chatbot-role"
-  slack_team_id    = "T00000000"
-  slack_channel_id = "C00000000"
+  configuration_name = "sample-alerts"
+  iam_role_arn       = "arn:aws:iam::123456789012:role/aws-chatbot-role"
+  slack_team_id      = "T00000000"
+  slack_channel_id   = "C00000000"
 
   sns_topic_arns = [
     "arn:aws:sns:eu-central-1:123456789012:sample-alerts"
